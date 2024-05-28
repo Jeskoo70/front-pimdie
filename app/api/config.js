2b3c24cd4e4fee5a5d391feb630d7d8e71071ee8
Jeskoo <gera.steam@mail.ru>
2024-05-28 20:12:44 +1000

    update url

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'http://localhost:3001/api'
-export const BASE_URL = 'https://jeskooisnotmakaka.nomorepartiesco.ru'
+export const BASE_URL = 'https://jeskooismakaka.nomorepartiesco.ru'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,

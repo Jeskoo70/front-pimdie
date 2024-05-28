8f103b54233b5556f9bbba4c87aa506a19f2479d
Jeskoo <gera.steam@mail.ru>
2024-05-28 20:06:27 +1000

    first commit

---
+++ app/api/config.js
@@ -0,0 +1,8 @@
+// export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'https://jeskooisnotmakaka.nomorepartiesco.ru'
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+}
